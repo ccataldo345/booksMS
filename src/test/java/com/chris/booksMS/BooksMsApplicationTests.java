c2123996c62161796c1126151de057366e1e03c4
Christian Cataldo <epost345@gmail.com>
2019-04-16 11:43:29 +0300

    Add Spring Initializer project

---
+++ src/test/java/com/chris/booksMS/BooksMsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chris.booksMS;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BooksMsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
