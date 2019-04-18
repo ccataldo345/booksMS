7d6f61e16967ec3de32c638e68bff3008c776a83
ccataldo345 <post345@outlook.com>
2019-04-18 16:56:16 +0300

    Add webjar bootstrap

--- src/main/java/com/chris/booksMS/controller/BookController.java
+++ src/main/java/com/chris/booksMS/controller/BookController.java
@@ -2,12 +2,24 @@ package com.chris.booksMS.controller;
 
 import com.chris.booksMS.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookController {
 
     @Autowired
     private BookRepository bookRepository;
+
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "index";
+    }
 
 }
