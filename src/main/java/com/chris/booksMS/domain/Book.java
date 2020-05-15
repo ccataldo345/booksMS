47f64f13bd2cadda21cf7d98ebc1bb6852b0b875
Christian Cataldo <epost345@gmail.com>
2020-05-15 16:07:34 +0300

    Fix Lombok dependecies in build.gradle configurations

--- src/main/java/com/chris/booksMS/domain/Book.java
+++ src/main/java/com/chris/booksMS/domain/Book.java
@@ -19,47 +19,4 @@ public class Book {
     @OneToMany
     @JoinColumn(name = "book_id")
     private List<Comment> comments;
-
-    public Book() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public List<Comment> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
-    }
 }
--- src/main/java/com/chris/booksMS/domain/Comment.java
+++ src/main/java/com/chris/booksMS/domain/Comment.java
@@ -14,32 +14,4 @@ public class Comment {
     private Long id;
     private Date date;
     private String commentText;
-
-    public Comment() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getCommentText() {
-        return commentText;
-    }
-
-    public void setCommentText(String commentText) {
-        this.commentText = commentText;
-    }
-
 }
